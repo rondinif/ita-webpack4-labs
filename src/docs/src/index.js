b47dcb2c7a9c877452e93fe52c73809d18cd7cf4
Franco Rondini <rondinif@yahoo.it>
2020-02-22 07:21:15 +0100

    docs(update): there are news

--- src/docs/src/index.js
+++ src/docs/src/index.js
@@ -1,4 +1,4 @@
 console.log('ita-webpack4-labs');
-let $api = document.querySelector('#api');
-$api.innerHTML =  process.env.PHYTOJS_API_SERVICE_URL;
+let $website = document.querySelector('#website');
+$website.innerHTML =  process.env.PHYTOJS_API_SERVICE_URL;
 
